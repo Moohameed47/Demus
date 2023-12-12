e0e93edfa06e89bb8c8b584dfa32aae0d97e7c06
Mohamed Tarek <108521860+Moohameed47@users.noreply.github.com>
2023-12-13 01:38:15 +0200

    Update Login.JS&Register.JS

--- JS/login.js
+++ JS/login.js
@@ -1,19 +1,27 @@
 let users = JSON.parse(localStorage.getItem('users')) || []
 
 function Login() {
-    let email = document.getElementById('InputEmail1').value
-    let password = document.getElementById('InputPassword1').value
-    let obj = {
-        "email": email,
-        "password": password,
-    }
-    if ((obj) => {
-        for (let i = 0; i < users.length; i++)
-            if (users[i].email === email && users[i].password === password)
-                return true
-        return false
-    }) {
-        alert('Login Successfully')
-        window.location.replace("http://localhost:63342/Demus/index.html");
-    } else alert('Invalid Email Or Password')
+    let email = document.getElementById('InputEmail1').value,
+        password = document.getElementById('InputPassword1').value;
+    if (email && password) {
+        let obj = {
+            "email": email,
+            "password": password,
+        }
+        if (exist(obj)) {
+            alert('Login Successfully')
+            window.location.replace("http://localhost:63342/Demus/index.html");
+        } else
+            alert('Invalid Email Or Password')
+    } else
+        alert("Please Enter The Data")
+}
+
+function exist(obj) {
+    console.log(users)
+    for (let i = 0; i < users.length; i++)
+        if (users[i].email === obj.email && users[i].password === obj.password) {
+            return true
+        }
+    return false
 }
--- JS/register.js
+++ JS/register.js
@@ -1,24 +1,33 @@
 let users = JSON.parse(localStorage.getItem('users')) || []
 
 function Register() {
-    let fName = document.getElementById('InputFName1').value,
-        lName = document.getElementById('InputLName1').value,
+    let fname = document.getElementById('InputFName1').value,
+        lname = document.getElementById('InputLName1').value,
         email = document.getElementById('InputEmail1').value,
         password = document.getElementById('InputPassword1').value;
-    let obj = {
-        "email": email,
-        "password": password,
-    }
-    if ((obj) => {
-        for (let i = 0; i < users.length; i++)
-            if (users[i].email === email && users[i].password === password)
-                return true
-        return false
-    }) {
-        alert('Register Successfully')
-        window.location.replace("http://localhost:63342/Demus/index.html");
+    if (fname && lname && email && password) {
+        let obj = {
+            "email": email,
+            "password": password,
+        }
+        if (exist(obj)) {
+            alert('Already Exist')
+        } else {
+            alert('Register Successfully')
+            users.push({email, password})
+            localStorage.setItem('users', JSON.stringify(users));
+            window.location.replace("http://localhost:63342/Demus/index.html");
+        }
     } else {
-        users.push({email, password})
-        localStorage.setItem('users', JSON.stringify(users));
+        alert("Please Enter The Data")
     }
+}
+
+function exist(obj) {
+    console.log(users)
+    for (let i = 0; i < users.length; i++)
+        if (users[i].email === obj.email && users[i].password === obj.password) {
+            return true
+        }
+    return false
 }
